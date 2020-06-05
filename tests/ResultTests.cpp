7e661786d352afc9b889a50f3abf1781bfc84b82
Lior Stern <liorst4@gmail.com>
2020-06-05 15:20:41 +0300

    use catch2 for tests.
    Implemented tests for or_, or_else, unwrap_or, unwrap_or_else, unwrap_or_default.

--- tests/ResultTests.cpp
+++ tests/ResultTests.cpp
@@ -1,109 +1,221 @@
 #include <memory>
 
-#include <gtest/gtest.h>
-
 #include <result/result.hpp>
 
-GTEST_TEST(resultTest, test_ok) {
-  const auto r = Result<int, int>{Ok(5)};
-  ASSERT_TRUE(r.is_ok());
-  ASSERT_FALSE(r.is_err());
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
-  const auto o = r.ok();
-  ASSERT_TRUE(o.has_value());
-
-  const auto e = r.err();
-  ASSERT_FALSE(e.has_value());
-
-  ASSERT_EQ(r.unwrap(), 5);
-  ASSERT_THROW(r.unwrap_err(), std::bad_variant_access);
-}
-
-GTEST_TEST(resultTest, test_err) {
-  const auto r = Result<int, int>{Err(5)};
-  ASSERT_FALSE(r.is_ok());
-  ASSERT_TRUE(r.is_err());
-
-  const auto o = r.ok();
-  ASSERT_FALSE(o.has_value());
-
-  const auto e = r.err();
-  ASSERT_TRUE(e.has_value());
-
-  ASSERT_EQ(r.unwrap_err(), 5);
-  ASSERT_THROW(r.unwrap(), std::bad_variant_access);
-}
-
-GTEST_TEST(resultTest, move) {
-  {
-    auto success = Result<std::string, int>{Ok("Hello world")};
-    std::string s = std::move(success.unwrap());
-    ASSERT_EQ(s, "Hello world");
-    ASSERT_NE(success.unwrap(), "Hello world");
-    ASSERT_EQ(success.unwrap(), "");
+SCENARIO("OK", "[result]") {
+  GIVEN("Result with Ok value of 5") {
+    const auto r = Result<int, int>{Ok(5)};
+    WHEN("Checking that its ok") {
+      THEN("Its ok") { REQUIRE(r.is_ok()); }
+    }
+    WHEN("Checking that its an error") {
+      THEN("Its not an error") { REQUIRE_FALSE(r.is_err()); }
+    }
+    WHEN("Taking its ok value") {
+      const auto maybe = r.ok();
+      THEN("It has that value") { REQUIRE(maybe.has_value()); }
+    }
+    WHEN("Taking its error value") {
+      const auto maybe = r.err();
+      THEN("It has no value") { REQUIRE_FALSE(maybe.has_value()); }
+    }
+    WHEN("Unwraping its ok value") {
+      THEN("It can be accsessed") {
+        int value = 0;
+        REQUIRE_NOTHROW(value = r.unwrap());
+        REQUIRE(value == 5);
+      }
+    }
+    WHEN("Unwraping its error value") {
+      THEN("It can't be accessed") {
+        int value = 0;
+        REQUIRE_THROWS(value = r.unwrap_err());
+      }
+    }
+    WHEN("Calling unwrap_or") {
+      THEN("Its the same as calling unwarp") {
+        REQUIRE(r.unwrap_or(6) == r.unwrap());
+      }
+    }
+    WHEN("Calling unwrap_or_default") {
+      THEN("Its the same as calling unwarp") {
+        REQUIRE(r.unwrap_or_default() == r.unwrap());
+      }
+    }
+    WHEN("Calling and_then") {
+      THEN("The given function is called") {
+        REQUIRE(r.and_then<int>([](const int &) -> Result<int, int> {
+                   return {Ok(3)};
+                 }).unwrap() == 3);
+      }
+    }
+    WHEN("Calling or_else") {
+      THEN("The given function is not called") {
+        REQUIRE(r.or_else<int>([](const int &) -> Result<int, int> {
+                   return {Ok(3)};
+                 }).unwrap() == 5);
+      }
+    }
+    WHEN("Calling unwrap_or_else") {
+      THEN("The given function is not called") {
+        REQUIRE(r.unwrap_or_else(([](const int &) -> int { return 3; })) == 5);
+      }
+    }
   }
-
-  {
-    auto failure = Result<int, std::string>{Err("Hello world")};
-    std::string s = std::move(failure.unwrap_err());
-    ASSERT_EQ(s, "Hello world");
-    ASSERT_NE(failure.unwrap_err(), "Hello world");
-    ASSERT_EQ(failure.unwrap_err(), "");
+  GIVEN("A move only ok value") {
+    auto success =
+        Result<std::unique_ptr<int>, int>{Ok(std::make_unique<int>(5))};
+    REQUIRE(5 == *success.unwrap());
+    WHEN("Its moved") {
+      std::unique_ptr<int> y = std::move(success.unwrap());
+      THEN("The value moved") {
+        REQUIRE(5 == *y);
+        REQUIRE(nullptr == success.unwrap());
+      }
+    }
   }
-
-  {
-    auto x = Result<std::unique_ptr<int>, int>{Ok(std::make_unique<int>(5))};
-    ASSERT_EQ(5, *x.unwrap());
-    std::unique_ptr<int> y = std::move(x.unwrap());
-    ASSERT_EQ(5, *y);
-    ASSERT_EQ(nullptr, x.unwrap());
- }
 }
 
-GTEST_TEST(resultTest, comparison) {
-  const auto x = Result<int, int>{Ok(5)};
-  ASSERT_EQ(x, x);
-  const auto y = Result<int, int>{Err(5)};
-  ASSERT_NE(x, y);
-  const auto z = Result<int, int>{Ok(6)};
-  ASSERT_NE(x, z);
-  ASSERT_NE(z, y);
-  ASSERT_EQ(z, z);
-  const auto k = Result<int, int>{Err(6)};
-  ASSERT_NE(y, k);
+SCENARIO("ERR", "[result]") {
+  GIVEN("Result with Err value of 5") {
+    const auto r = Result<int, int>{Err(5)};
+    WHEN("Checking that its ok") {
+      THEN("Its not ok") { REQUIRE_FALSE(r.is_ok()); }
+    }
+    WHEN("Checking that its an error") {
+      THEN("Its an error") { REQUIRE(r.is_err()); }
+    }
+    WHEN("Taking its ok value") {
+      const auto maybe = r.ok();
+      THEN("It has no value") { REQUIRE_FALSE(maybe.has_value()); }
+    }
+    WHEN("Taking its error value") {
+      const auto maybe = r.err();
+      THEN("It has that value") { REQUIRE(maybe.has_value()); }
+    }
+    WHEN("Unwraping its ok value") {
+      THEN("It can't be accessed") {
+        int value = 0;
+        REQUIRE_THROWS(value = r.unwrap());
+      }
+    }
+    WHEN("Unwraping its error value") {
+      THEN("It can be accsessed") {
+        int value = 0;
+        REQUIRE_NOTHROW(value = r.unwrap_err());
+        REQUIRE(value == 5);
+      }
+    }
+    WHEN("Calling unwrap_or") {
+      THEN("The default value is returned") { REQUIRE(r.unwrap_or(6) == 6); }
+    }
+    WHEN("Calling unwrap_or_default") {
+      THEN("The default value is returned") {
+        REQUIRE(r.unwrap_or_default() == int{});
+      }
+    }
+    WHEN("Calling and_then") {
+      THEN("The given function is not called") {
+        REQUIRE(r.and_then<int>([](const int &) -> Result<int, int> {
+                   return {Ok(3)};
+                 }).unwrap_err() == 5);
+      }
+    }
+    WHEN("Calling or_else") {
+      THEN("The given function is called") {
+        REQUIRE(r.or_else<int>([](const int &) -> Result<int, int> {
+                   return {Ok(3)};
+                 }).unwrap() == 3);
+      }
+    }
+    WHEN("Calling unwrap_or_else") {
+      THEN("The given function is called") {
+        REQUIRE(r.unwrap_or_else(([](const int &) -> int { return 3; })) == 3);
+      }
+    }
+  }
+  GIVEN("A move only err value") {
+    auto failure =
+        Result<int, std::unique_ptr<int>>{Err(std::make_unique<int>(5))};
+    REQUIRE(5 == *failure.unwrap_err());
+    WHEN("Its moved") {
+      std::unique_ptr<int> y = std::move(failure.unwrap_err());
+      THEN("The value moved") {
+        REQUIRE(5 == *y);
+        REQUIRE(nullptr == failure.unwrap_err());
+      }
+    }
+  }
 }
 
-GTEST_TEST(resultTest, and_) {
-  const auto r = Result<int, int>{Err(7)};
-  const auto l = Result<int, int>{Ok(7)};
-  const auto h = Result<int, int>{Err(8)};
-  ASSERT_EQ(r.and_(l), r);
-  ASSERT_EQ(l.and_(r), r);
-  ASSERT_NE(r.and_(h), h);
+SCENARIO("Interaction", "[result]") {
+  GIVEN("Two oks with the same values") {
+    const auto a = Result<int, int>{Ok(5)};
+    const auto b = Result<int, int>{Ok(5)};
+    WHEN("Compared") {
+      THEN("They are equal") { REQUIRE(a == b); }
+      THEN("They are not unequal") { REQUIRE_FALSE(a != b); }
+    }
+  }
+  GIVEN("Two oks with different values") {
+    const auto a = Result<int, int>{Ok(5)};
+    const auto b = Result<int, int>{Ok(6)};
+    WHEN("Compared") {
+      THEN("They are not equal") { REQUIRE_FALSE(a == b); }
+      THEN("They are unequal") { REQUIRE(a != b); }
+    }
+    REQUIRE(a.and_(b) == b);
+    REQUIRE(a.or_(b) == a);
+    REQUIRE(b.and_(a) == a);
+    REQUIRE(b.or_(a) == b);
+  }
+  GIVEN("Two errs with the same values") {
+    const auto a = Result<int, int>{Err(5)};
+    const auto b = Result<int, int>{Err(5)};
+    WHEN("Compared") {
+      THEN("They are equal") { REQUIRE(a == b); }
+      THEN("They are not unequal") { REQUIRE_FALSE(a != b); }
+    }
+  }
+  GIVEN("Two errs with different values") {
+    const auto a = Result<int, int>{Err(5)};
+    const auto b = Result<int, int>{Err(6)};
+    WHEN("Compared") {
+      THEN("They are not equal") { REQUIRE_FALSE(a == b); }
+      THEN("They are unequal") { REQUIRE(a != b); }
+    }
+    REQUIRE(a.and_(b) == a);
+    REQUIRE(a.or_(b) == b);
+    REQUIRE(b.and_(a) == b);
+    REQUIRE(b.or_(a) == a);
+  }
+  GIVEN("Ok and Err with same values") {
+    const auto a = Result<int, int>{Ok(5)};
+    const auto b = Result<int, int>{Err(5)};
+    WHEN("Compared") {
+      THEN("They are not equal") { REQUIRE_FALSE(a == b); }
+      THEN("They are unequal") { REQUIRE(a != b); }
+    }
+    REQUIRE(a.and_(b) == b);
+    REQUIRE(a.or_(b) == a);
+    REQUIRE(b.and_(a) == b);
+    REQUIRE(b.or_(a) == a);
+  }
+  GIVEN("Ok and Err with different values") {
+    const auto a = Result<int, int>{Ok(5)};
+    const auto b = Result<int, int>{Err(6)};
+    WHEN("Compared") {
+      THEN("They are not equal") { REQUIRE_FALSE(a == b); }
+      THEN("They are unequal") { REQUIRE(a != b); }
+    }
+    REQUIRE(a.and_(b) == b);
+    REQUIRE(a.or_(b) == a);
+    REQUIRE(b.and_(a) == b);
+    REQUIRE(b.or_(a) == a);
+  }
 }
 
-GTEST_TEST(resultTest, and_then) {
-  const auto e = Result<int, int>{Err(7)};
-  const auto o = Result<int, int>{Ok(6)};
-
-  const auto x =
-      e.and_then<int>([](const int &) -> Result<int, int> { return {Ok(5)}; });
-  ASSERT_TRUE(x.is_err());
-
-  const auto y =
-      o.and_then<int>([](const int &) -> Result<int, int> { return {Ok(5)}; });
-  ASSERT_TRUE(y.is_ok());
-  ASSERT_EQ(5, y.unwrap());
-}
-
-// TODO: Test or_
-// TODO: Test or_else
 // TODO: Test TRY_ASSIGNMENT.
-// TODO: Test unwrap_or
-// TODO: Test unwrap_or_else
-// TODO: Test unwrap_or_default
-
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
